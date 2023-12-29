10255fd1e13f7ccb621d0d7b00eb5fe761deb174
Your Name <you@example.com>
2023-12-29 11:19:00 -0600

    Fixing component

--- src/DnDContext/DnDContext.stories.ts
+++ src/DnDContext/DnDContext.stories.ts
@@ -5,7 +5,6 @@ import DnDContext from './DnDContext';
 const meta: Meta<typeof DnDContext> = {
     title: 'DnDContext',
     component: DnDContext,
-    tags: ['autodocs'],
     parameters: {
         // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'centered',
